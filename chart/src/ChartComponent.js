93b2b500755361a696b80ae3c5a22b17f86af0d0
joechea <s.chea@aupp.edu.kh>
2024-04-25 16:58:31 +0700

    initial push

---
+++ chart/src/ChartComponent.js
@@ -0,0 +1,40 @@
+import React, {useEffect, createRef} from "react";
+import Chart from "chart.js";
+
+const ChartComponent = ({data, width, height}) => {
+  const canvasRef = createRef();
+  useEffect(() => {
+    const ctx = canvasRef.current;
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: data.map(d => d.x),
+        datasets: [
+          {
+            label: "Max. Temperature (in degrees Celcius)",
+            data: data.map(d => d.y),
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: false
+              }
+            }
+          ]
+        }
+      }
+    });
+  });
+  return (
+    <canvas id="chart-canvas" width={width} height={height} ref={canvasRef} />
+  );
+};
+
+export default ChartComponent;
